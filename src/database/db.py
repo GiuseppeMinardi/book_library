19c70b18565b4a97a2fd67cbb6fb58aca1c1a442
GiuseppeMinardi <38011429+GiuseppeMinardi@users.noreply.github.com>
2026-03-04 22:09:49 +0100

    feat: implement database schema creation in SQL file and refactor db.py to use it

--- src/database/db.py
+++ src/database/db.py
@@ -14,6 +14,7 @@ from ..models.project_paths import ProjectPathsSettings
 
 project_paths = ProjectPathsSettings()
 
+_query_folder = project_paths.database_path.joinpath("sql")
 
 class Database:
     """Class to connect and handle the sl=qllite database."""
@@ -67,92 +68,7 @@ class Database:
         try:
             cursor = self.conn.cursor()
             # Book Table
-            cursor.execute("""
-                CREATE TABLE IF NOT EXISTS books (
-                    id TEXT PRIMARY KEY,
-                    title TEXT NOT NULL,
-                    publisher TEXT,
-                    publishedDate TEXT,
-                    description TEXT,
-                    pageCount INTEGER,
-                    printType TEXT,
-                    language TEXT,
-                    infoLink TEXT,
-                    smallThumbnail TEXT,
-                    isbn TEXT UNIQUE NOT NULL
-                );
-            """)
-
-            # Authors Table
-            cursor.execute("""
-                CREATE TABLE IF NOT EXISTS authors (
-                    id TEXT PRIMARY KEY,
-                    name TEXT NOT NULL UNIQUE,
-                    birth_date TEXT,
-                    death_date TEXT,
-                    nationality TEXT,
-                    sex TEXT,
-                    bio TEXT,
-                    author_link TEXT
-                );
-            """)
-
-            # Categories Table
-            cursor.execute("""
-                CREATE TABLE IF NOT EXISTS categories (
-                    id TEXT PRIMARY KEY,
-                    name TEXT NOT NULL UNIQUE
-                );
-            """)
-
-            # Book-Author Link Table (Many-to-Many)
-            cursor.execute("""
-                CREATE TABLE IF NOT EXISTS book_authors (
-                    book_id TEXT,
-                    author_id TEXT,
-                    PRIMARY KEY (book_id, author_id),
-                    FOREIGN KEY (book_id) REFERENCES books (id) ON DELETE CASCADE,
-                    FOREIGN KEY (author_id) REFERENCES authors (id) ON DELETE CASCADE
-                );
-            """)
-
-            # Book-Category Link Table (Many-to-Many)
-            cursor.execute("""
-                CREATE TABLE IF NOT EXISTS book_categories (
-                    book_id TEXT,
-                    category_id TEXT,
-                    PRIMARY KEY (book_id, category_id),
-                    FOREIGN KEY (book_id) REFERENCES books (id) ON DELETE CASCADE,
-                    FOREIGN KEY (category_id) REFERENCES categories (id) ON DELETE CASCADE
-                );
-            """)
-
-            # Embeddings Table
-            cursor.execute("""
-                CREATE TABLE IF NOT EXISTS book_embeddings (
-                    book_id TEXT,
-                    model_name TEXT,
-                    vector BLOB,
-                    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                    PRIMARY KEY (book_id, model_name),
-                    FOREIGN KEY (book_id) REFERENCES books (id) ON DELETE CASCADE
-                );
-            """)
-            self.conn.commit()
-
-            # author embeddings
-            cursor.execute(
-                """
-                CREATE TABLE IF NOT EXISTS author_embeddings (
-                    author_id TEXT,
-                    model_name TEXT,
-                    vector BLOB,
-                    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                    PRIMARY KEY (author_id, model_name),
-                    FOREIGN KEY (author_id) REFERENCES authors (id) ON DELETE CASCADE
-                );
-            """
-            )
+            cursor.execute(_query_folder.joinpath("create_db.sql").read_text())
             logger.info("Tables created successfully.")
         except sqlite3.Error as e:
             logger.info(f"Error creating tables: {e}")
